58cf57f871adefd8b9da480646a103907e214c81
JPedro99 <pedrocasmg@hotmail.com>
2020-01-13 23:07:34 +0000

    1 aula

---
+++ Assets/Scrpits/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Rigidbody2D selfrigidbody;
+    private bool moveleft;
+    private bool moveright;
+    public float speed;
+
+    void Start()
+    {
+        selfrigidbody = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveleft =Input.GetKey(KeyCode.A);
+        moveright = Input.GetKey(KeyCode.D);
+
+    }
+
+    void FixedUpdate()
+    {
+        selfrigidbody.velocity = new Vector2();
+        float currspeed = 0;
+        if (moveleft) currspeed -= speed;
+        if (moveright) currspeed += speed;
+        selfrigidbody.velocity = new Vector2(currspeed, selfrigidbody.velocity.y);
+    }
+}
